68717c77152ebf30c2c691637fe76cce6ccd633d
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-25 18:31:32 +0530

    Add Terraform file aws/20260325-130129-i-need-to-create-an-aws-s3-storage-bucke-c5a186dc/variables.tf

---
+++ aws/20260325-130129-i-need-to-create-an-aws-s3-storage-bucke-c5a186dc/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region to create the S3 bucket in."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+  default     = "bucket_ishika"
+}
+
+variable "acl" {
+  description = "The ACL to apply to the bucket."
+  type        = string
+  default     = "public-read"
+}
