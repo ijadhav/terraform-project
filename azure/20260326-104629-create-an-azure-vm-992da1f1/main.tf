3df2cc5cf021d6acf46fe46e9420861a95c41708
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 16:18:19 +0530

    Update Terraform file azure/20260326-104629-create-an-azure-vm-992da1f1/main.tf

--- azure/20260326-104629-create-an-azure-vm-992da1f1/main.tf
+++ azure/20260326-104629-create-an-azure-vm-992da1f1/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                = "example-vm"
+  name                = "Ishika"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   size                = "Standard_B1s"
