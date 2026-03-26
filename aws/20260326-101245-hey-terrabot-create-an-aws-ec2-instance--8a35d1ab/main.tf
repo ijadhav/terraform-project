849fa6818cda575d09fadfaa90074deebc9d3635
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 15:42:47 +0530

    Update Terraform file aws/20260326-101245-hey-terrabot-create-an-aws-ec2-instance--8a35d1ab/main.tf

---
+++ aws/20260326-101245-hey-terrabot-create-an-aws-ec2-instance--8a35d1ab/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "nginx" {
+  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo amazon-linux-extras install nginx1 -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "nginx-server"
+  }
+}
