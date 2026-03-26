a4679e7c2d6906b2fd4859e2a5a7677901418e58
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 18:15:52 +0530

    Update Terraform file azure/20260326-123632-create-an-azure-vm-2cda01d4/main.tf

--- azure/20260326-123632-create-an-azure-vm-2cda01d4/main.tf
+++ azure/20260326-123632-create-an-azure-vm-2cda01d4/main.tf
@@ -69,6 +69,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_public_ip" "public_ip" {
