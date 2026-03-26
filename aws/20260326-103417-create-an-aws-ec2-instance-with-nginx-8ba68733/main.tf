4e0f4d5a42df607581bf4db9e6a68e025a0b2ae7
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 16:08:04 +0530

    Update Terraform file aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf

--- aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf
+++ aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf
@@ -1,13 +1,28 @@
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "CustomVPC"
+  }
+}
+
+resource "aws_subnet" "custom_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "CustomSubnet"
+  }
+}
+
 resource "aws_instance" "nginx" {
   ami           = "ami-08541b76eeb39c6e7"
   instance_type = "t2.micro"
-
+  subnet_id     = aws_subnet.custom_subnet.id
   user_data = <<-EOF
     #!/bin/bash
     # No additional setup commands
   EOF
-
   tags = {
-    Name = "UbuntuServer"
+    Name = "Ishika"
   }
 }
