fb5f0d4f9275affad244861af28da83536528501
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 16:05:10 +0530

    Update Terraform file aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf

--- aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf
+++ aws/20260326-103417-create-an-aws-ec2-instance-with-nginx-8ba68733/main.tf
@@ -1,15 +1,13 @@
 resource "aws_instance" "nginx" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-08541b76eeb39c6e7"
   instance_type = "t2.micro"
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo amazon-linux-extras install -y nginx1
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
+    # No additional setup commands
   EOF
 
   tags = {
-    Name = "NginxServer"
+    Name = "UbuntuServer"
   }
 }
