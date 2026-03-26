ae235e02b11d46fce438afd3bbabaebbe7486997
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 18:40:17 +0530

    Update Terraform file aws/20260326-131015-create-an-aws-ec2-instance-d38415cc/main.tf

---
+++ aws/20260326-131015-create-an-aws-ec2-instance-d38415cc/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1; update as needed
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "example-ec2-instance"
+  }
+}
