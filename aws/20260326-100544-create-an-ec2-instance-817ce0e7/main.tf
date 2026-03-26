1858b28c50a24e697c5e1b8d4f2cfa2d2e2a1379
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 15:36:18 +0530

    Update Terraform file aws/20260326-100544-create-an-ec2-instance-817ce0e7/main.tf

--- aws/20260326-100544-create-an-ec2-instance-817ce0e7/main.tf
+++ aws/20260326-100544-create-an-ec2-instance-817ce0e7/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
   tags = {
-    Name = "ExampleInstance"
+    Name = "Ishi-Test"
   }
 }
