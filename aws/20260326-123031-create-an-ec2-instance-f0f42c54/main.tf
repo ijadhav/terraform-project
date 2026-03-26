cf824f763abc88adff763a0b2293783b1a1c0650
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 18:00:33 +0530

    Update Terraform file aws/20260326-123031-create-an-ec2-instance-f0f42c54/main.tf

---
+++ aws/20260326-123031-create-an-ec2-instance-f0f42c54/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+resource "aws_security_group" "ec2_ssh" {
+  name        = "ec2-ssh-sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.ec2_ssh.id]
+
+  tags = {
+    Name = "example-ec2-instance"
+  }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
