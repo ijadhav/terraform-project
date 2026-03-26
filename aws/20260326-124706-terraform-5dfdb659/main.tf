60b3d5af44e6bce2b64ab83d851e49fd5ae83af0
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-26 18:17:08 +0530

    Update Terraform file aws/20260326-124706-terraform-5dfdb659/main.tf

---
+++ aws/20260326-124706-terraform-5dfdb659/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
