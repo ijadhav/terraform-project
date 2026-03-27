0422b4a01dcb6f146b61e300be2e9b4a7803bb22
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-27 12:40:13 +0530

    Update Terraform file aws/20260327-071011-create-an-aws-ec2-instance-d38415cc/main.tf

---
+++ aws/20260327-071011-create-an-aws-ec2-instance-d38415cc/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_basic_sg"
+  description = "Security group for basic EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "ec2-basic-sg"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "example-ec2-instance"
+  }
+}
