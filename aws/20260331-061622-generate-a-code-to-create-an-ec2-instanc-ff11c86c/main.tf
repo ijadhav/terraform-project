d7566f1acf535a59c8882ffdda7232cfcc312f3c
Ishika Jadhav <ijadhav@venacorp.com>
2026-03-31 11:49:30 +0530

    Update Terraform file aws/20260331-061622-generate-a-code-to-create-an-ec2-instanc-ff11c86c/main.tf

--- aws/20260331-061622-generate-a-code-to-create-an-ec2-instanc-ff11c86c/main.tf
+++ aws/20260331-061622-generate-a-code-to-create-an-ec2-instanc-ff11c86c/main.tf
@@ -16,6 +16,15 @@ resource "aws_instance" "example" {
   ami           = "ami-0123456789abcdef0"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y || apt-get update -y
+              # Try installing nginx with yum (Amazon Linux/RHEL/CentOS) then fallback to apt (Ubuntu/Debian)
+              yum install -y nginx || apt-get install -y nginx
+              systemctl enable nginx || update-rc.d nginx defaults
+              systemctl start nginx || service nginx start
+              EOF
+
   tags = {
     Name = "example-ec2-instance"
   }
