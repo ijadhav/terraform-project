ddd4cf582898baead197f9f29e8fdaba8b6f9fbe
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-01 16:52:24 +0530

    Update Terraform file aws/20260401-110636-create-an-ec2-instance-with-existing-mod-d7ca014d/variables.tf

--- aws/20260401-110636-create-an-ec2-instance-with-existing-mod-d7ca014d/variables.tf
+++ aws/20260401-110636-create-an-ec2-instance-with-existing-mod-d7ca014d/variables.tf
@@ -5,9 +5,9 @@ variable "aws_region" {
 }
 
 variable "ec2_instance_name" {
-  description = "Name tag for the EC2 instance"
+  description = "Name tag for the EC2 instance with NGINX"
   type        = string
-  default     = "sandbox-ec2"
+  default     = "sandbox-ec2-nginx"
 }
 
 variable "ec2_instance_type" {
@@ -23,7 +23,19 @@ variable "ec2_module_source" {
 }
 
 variable "ec2_user_data" {
-  description = "User data script to run on EC2 instance bootstrap"
+  description = "User data script to run on EC2 instance bootstrap (for NGINX instance)"
   type        = string
   default     = "#!/bin/bash\n\nyum update -y || apt-get update -y\n\n# Install nginx for Amazon Linux / RHEL-based\nyum install -y nginx || apt-get install -y nginx\n\n# Enable and start nginx\nsystemctl enable nginx || true\nsystemctl start nginx || service nginx start\n"
+}
+
+variable "ec2_basic_instance_name" {
+  description = "Name tag for the basic EC2 instance without extras"
+  type        = string
+  default     = "sandbox-ec2-basic"
+}
+
+variable "ec2_basic_instance_type" {
+  description = "EC2 instance type for the basic instance"
+  type        = string
+  default     = "t3.micro"
 }
