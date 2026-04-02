0bd0e0b7ef701f2a64e6850c2fdfdbfee0ed660b
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-02 17:19:31 +0530

    [AWS] Update Terraform file aws/changes/ba7a180d6974/main.tf

--- aws/changes/ba7a180d6974/main.tf
+++ aws/changes/ba7a180d6974/main.tf
@@ -14,11 +14,38 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
   associate_public_ip_address = true
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = var.s3_bucket_environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "storage_versioning" {
+  bucket = aws_s3_bucket.storage.id
+
+  versioning_configuration {
+    status = var.s3_bucket_versioning_enabled ? "Enabled" : "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "storage_sse" {
+  bucket = aws_s3_bucket.storage.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
