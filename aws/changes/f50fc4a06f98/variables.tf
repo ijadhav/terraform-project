045db55ea573627a5d7d5d30e5f280925758e67e
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-02 12:50:18 +0530

    Update Terraform file aws/changes/f50fc4a06f98/variables.tf

---
+++ aws/changes/f50fc4a06f98/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EC2 instance will be created"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name for the EC2 key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Public SSH key material for the key pair"
+  type        = string
+}
