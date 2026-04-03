0db2cb62ae4b3841e65bdc8be2db388f6960a54c
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-03 14:04:36 +0530

    [AWS] Update Terraform file aws/changes/574094d2e346/variables.tf

---
+++ aws/changes/574094d2e346/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance (e.g., Amazon Linux 2)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EC2 instance and security group will be created"
+  type        = string
+}
