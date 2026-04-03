7e945b3e51302b44a277178009f9c8202787a315
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-03 17:24:12 +0530

    [AZURE] Update Terraform file azure/changes/4851a0a99a74/outputs.tf

--- azure/changes/4851a0a99a74/outputs.tf
+++ azure/changes/4851a0a99a74/outputs.tf
@@ -12,3 +12,18 @@ output "mongo_exports_container_name" {
   description = "Name of the MongoDB exports container"
   value       = azurerm_storage_container.mongo_exports.name
 }
+
+output "general_storage_account_id" {
+  description = "Resource ID of the general purpose storage account"
+  value       = azurerm_storage_account.general_storage.id
+}
+
+output "general_data_container_name" {
+  description = "Name of the general data container"
+  value       = azurerm_storage_container.general_data.name
+}
+
+output "general_logs_container_name" {
+  description = "Name of the general logs container"
+  value       = azurerm_storage_container.general_logs.name
+}
