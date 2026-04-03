232cbeffd36efc6a2fe7924fcdf5e5cec94568e0
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-03 17:24:11 +0530

    [AZURE] Update Terraform file azure/changes/4851a0a99a74/main.tf

--- azure/changes/4851a0a99a74/main.tf
+++ azure/changes/4851a0a99a74/main.tf
@@ -48,3 +48,35 @@ resource "azurerm_storage_container" "mongo_exports" {
   storage_account_name  = azurerm_storage_account.mongo_storage.name
   container_access_type = "private"
 }
+
+# General-purpose storage account (S3-like)
+resource "azurerm_storage_account" "general_storage" {
+  name                     = var.general_storage_account_name
+  resource_group_name      = azurerm_resource_group.mongo_rg.name
+  location                 = azurerm_resource_group.mongo_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  enable_https_traffic_only = true
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "general_data" {
+  name                  = "data"
+  storage_account_name  = azurerm_storage_account.general_storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "general_logs" {
+  name                  = "logs"
+  storage_account_name  = azurerm_storage_account.general_storage.name
+  container_access_type = "private"
+}
