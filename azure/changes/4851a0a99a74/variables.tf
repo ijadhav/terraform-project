be208aef955ae0e2b690c6e7442708384767b0a7
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-03 17:24:11 +0530

    [AZURE] Update Terraform file azure/changes/4851a0a99a74/variables.tf

--- azure/changes/4851a0a99a74/variables.tf
+++ azure/changes/4851a0a99a74/variables.tf
@@ -9,11 +9,16 @@ variable "mongo_storage_account_name" {
   description = "Globally unique name for the MongoDB storage account"
 }
 
+variable "general_storage_account_name" {
+  type        = string
+  description = "Globally unique name for the general purpose storage account (S3-like)"
+}
+
 variable "tags" {
   type        = map(string)
-  description = "Tags to apply to MongoDB storage resources"
+  description = "Tags to apply to storage resources"
   default = {
     environment = "dev"
-    workload    = "mongodb"
+    workload    = "storage"
   }
 }
