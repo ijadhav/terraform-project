38516d6adbd8c590417399ef8e1c9afe80923081
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-05 21:38:59 +0530

    [AWS] Update Terraform file aws/changes/53f050f15779/main.tf

--- aws/changes/53f050f15779/main.tf
+++ aws/changes/53f050f15779/main.tf
@@ -13,10 +13,39 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_security_group" "test_http" {
+  name        = "test-ec2-http-sg"
+  description = "Security group for test EC2 instance allowing HTTP"
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    {
+      Name = "test-ec2-http-sg"
+    },
+    var.default_tags,
+  )
+}
+
 resource "aws_instance" "test" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
   associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.test_http.id]
 
   tags = merge(
     {
