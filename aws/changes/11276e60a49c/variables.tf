e15fe5b1d8f4e3815e1b25e7dc953fb3918ffead
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-05 13:31:24 +0530

    [AWS] Update Terraform file aws/changes/11276e60a49c/variables.tf

--- aws/changes/11276e60a49c/variables.tf
+++ aws/changes/11276e60a49c/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWS region in which to create the S3 bucket"
+  description = "AWS region in which to create resources"
   type        = string
   default     = "us-east-1"
 }
@@ -10,7 +10,7 @@ variable "bucket_name" {
 }
 
 variable "environment" {
-  description = "Environment tag for the S3 bucket"
+  description = "Environment tag for created resources"
   type        = string
   default     = "dev"
 }
@@ -22,7 +22,38 @@ variable "enable_versioning" {
 }
 
 variable "tags" {
-  description = "Additional tags to apply to the S3 bucket"
+  description = "Additional tags to apply to resources"
   type        = map(string)
   default     = {}
+}
+
+# New variables for VPC, subnet, and EC2 instance
+
+variable "vpc_cidr" {
+  description = "CIDR block for the custom VPC named test"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the custom subnet named test"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet (must belong to aws_region)"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the Ishika EC2 instance (e.g., Amazon Linux 2 or Ubuntu)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for Ishika"
+  type        = string
+  default     = "t3.micro"
 }
