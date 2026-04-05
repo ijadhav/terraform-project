3a2116ecdd22e5c0514fb64c9bfba63d1ddebce0
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-05 21:38:10 +0530

    [AWS] Update Terraform file aws/changes/53f050f15779/main.tf

---
+++ aws/changes/53f050f15779/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "test" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  associate_public_ip_address = true
+
+  tags = merge(
+    {
+      Name = "test-ec2-instance"
+    },
+    var.default_tags,
+  )
+}
