6d7fce597f0474b9e6e43cda3a1ca7cff844365d
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-06 14:09:47 +0530

    [AZURE] Update Terraform file azure/changes/76630a9f29b9/variables.tf

---
+++ azure/changes/76630a9f29b9/variables.tf
@@ -0,0 +1,30 @@
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-mongo-storage"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the storage account (3-24 lowercase letters and numbers)"
+  type        = string
+}
+
+variable "cosmos_account_name" {
+  description = "Name for the Cosmos DB account (must be globally unique, 3-44 lowercase letters and numbers)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    environment = "dev"
+    project     = "mongodb-storage"
+  }
+}
