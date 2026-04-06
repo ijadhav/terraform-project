6f9e57274a428006c40accb1d4d49d6bac6f2622
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-06 14:59:12 +0530

    [AWS] Update Terraform file aws/changes/53f050f15779/main.tf

--- aws/changes/53f050f15779/main.tf
+++ aws/changes/53f050f15779/main.tf
@@ -25,3 +25,23 @@ resource "aws_instance" "test" {
     var.default_tags,
   )
 }
+
+resource "aws_s3_bucket" "test" {
+  bucket = var.s3_bucket_name
+
+  tags = merge(
+    {
+      Name = "test-s3-bucket"
+    },
+    var.default_tags,
+  )
+}
+
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
