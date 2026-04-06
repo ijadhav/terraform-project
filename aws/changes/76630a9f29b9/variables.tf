444bc0aac2ec2803bdfc3ccf7f3bc60ef553f14f
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-06 14:00:38 +0530

    [AWS] Update Terraform file aws/changes/76630a9f29b9/variables.tf

---
+++ aws/changes/76630a9f29b9/variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_ami" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be placed"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to associate with the EC2 instance"
+  type        = list(string)
+  default     = []
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "example-ec2-instance"
+}
