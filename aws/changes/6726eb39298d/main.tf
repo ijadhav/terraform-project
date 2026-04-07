33a01cff720cffea74034758ec61ce6d24a89313
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-07 11:04:30 +0530

    [AWS] Update Terraform file aws/changes/6726eb39298d/main.tf

---
+++ aws/changes/6726eb39298d/main.tf
@@ -0,0 +1,18 @@
+{
+  "provider": {
+    "aws": {
+      "region": "us-east-1"
+    }
+  },
+  "resource": {
+    "aws_instance": {
+      "ec2": {
+        "ami": "ami-0c55b159cbfafe1f0",
+        "instance_type": "t2.micro",
+        "tags": {
+          "Name": "ec2"
+        }
+      }
+    }
+  }
+}
