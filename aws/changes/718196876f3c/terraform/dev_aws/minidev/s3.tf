4b9b3fa3e016d42e30202f601a1c96cd485cee61
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-14 12:10:42 +0530

    [AWS] Update Terraform file aws/changes/718196876f3c/terraform/dev_aws/minidev/s3.tf

--- aws/changes/718196876f3c/terraform/dev_aws/minidev/s3.tf
+++ aws/changes/718196876f3c/terraform/dev_aws/minidev/s3.tf
@@ -1,7 +1,7 @@
 module "s3" {
   source = "../../modules/s3"
 
-  bucket_name = var.bucket_name
+  bucket_name = "test"
   glacier_transition_days = var.glacier_transition_days
   expiration_days = var.expiration_days
   sse_enabled = var.sse_enabled
