f5a6b3930cd5106042d038d46e1f9969f3ccb7b9
Ishika Jadhav <ijadhav@venacorp.com>
2026-04-15 14:29:55 +0530

    [AWS] Update Terraform file aws/changes/288b904ac57d/terraform/dev_aws/minidev/main.tf

---
+++ aws/changes/288b904ac57d/terraform/dev_aws/minidev/main.tf
@@ -0,0 +1,11 @@
+module "redshift_cluster" {
+  source = "../../modules/redshift_cluster"
+  cluster_identifier = var.cluster_identifier
+  database_name = var.database_name
+  master_username = var.master_username
+  master_password = var.master_password
+  node_type = var.node_type
+  cluster_subnet_group_name = var.cluster_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  iam_roles = var.iam_roles
+}
